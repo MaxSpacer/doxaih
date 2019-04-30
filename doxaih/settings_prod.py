8c113ecb3a0a7b1db30416c8dc99ea6977418cb9
MaxSpacer <max_spacer@mail.ru>
2019-04-30 15:57:48 +0300

    start 0.1

--- doxaih/settings_prod.py
+++ doxaih/settings_prod.py
@@ -7,7 +7,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'likwid_db1',
         'USER': 'django_likwid',
-        'PASSWORD': 'Dl123456',
+        'PASSWORD': 'vftv6iI&',
         'HOST': 'localhost',
         'PORT': '',                      # Set to empty string for default.
     }
@@ -17,7 +17,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_PORT = '1025'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = "info@likwid.club"
+EMAIL_HOST_USER = "info@.club"
 EMAIL_HOST_PASSWORD = "Il123456!"
 EMAIL_USE_SSL = True
 # SERVER_EMAIL = EMAIL_HOST_USER
