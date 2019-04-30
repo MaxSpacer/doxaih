6d84b8ba60e056a23f66847f4d5db6dff785ec50
MaxSpacer <max_spacer@mail.ru>
2019-04-30 16:00:01 +0300

    0.11

--- doxaih/settings.py
+++ doxaih/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nroz4=s6m7iik7dgqg3s$vt4j7bx^t!o5m*6b7(#b!+235$v_#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['doxaih.ru']
 
 
 # Application definition
