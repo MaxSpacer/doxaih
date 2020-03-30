e601cc026c7202495f9bbbb29af18bc79636744c
MaxSpacer <max_spacer@mail.ru>
2020-03-30 03:55:37 +0300

    up

--- landing/admin.py
+++ landing/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Callmecontact
+from .models import Callmecontact, Landpost
 from .models import Landpost
 
 
-class CallmecontactAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Callmecontact._meta.fields]
-admin.site.register(Callmecontact, CallmecontactAdmin)
+# class CallmecontactAdmin(admin.ModelAdmin):
+#     list_display = [field.name for field in Callmecontact._meta.fields]
+# admin.site.register(Callmecontact, CallmecontactAdmin)
+
+class LandpostAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Landpost._meta.fields]
+admin.site.register(Landpost, LandpostAdmin)
